134dd57025894be20cf0d1e0470f9e3904b34161
Angelo Carly <angelo.carly@protonmail.com>
2024-09-29 22:47:15 +0200

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,89 @@
-use crate::Instruction::Print;
+use crate::Instruction::{Load, Read, Return, Write};
 
 enum Instruction {
-    Print(i8)
+    Load(bool),
+    Read(i8),
+    Write(i8),
+    Return,
 }
 
 struct Machine {
     memory: [bool; 256],
+    register: bool,
 }
 
 impl Machine {
     fn new() -> Machine {
         Machine {
             memory: [false; 256],
+            register: false
         }
     }
 
-    fn run(&mut self, instructions: &[Instruction]) {
+    fn run(&mut self, instructions: &[Instruction]) -> Option<bool> {
         for instruction in instructions {
             match instruction {
-                Print(i) => {
-                    println!("Memory at {}: {}", i, self.memory[*i as usize]);
+                Load(v) => {
+                    self.register = *v;
                 },
+                Read(i) => {
+                    self.register = self.memory[*i as usize]
+                },
+                Write(i) => {
+                    self.memory[*i as usize] = self.register;
+                },
+                Return=> {
+                    return Some(self.register);
+                }
             }
         }
+
+        None
     }
 }
 
 fn main() {
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn test_instruction() {
-    let mut m = Machine::new();
-    let instructions = [
-        Print(0),
-        Print(1),
-    ];
-    m.run(&instructions);
+    #[test]
+    fn test_no_return_gives_none() {
+        let instructions = [];
+        let result = Machine::new().run(&instructions);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_return_gives_register() {
+        let instructions = [
+            Return
+        ];
+        let result = Machine::new().run(&instructions);
+        assert_eq!(result, Some(false));
+    }
+
+    #[test]
+    fn test_return_gives_loaded_register() {
+        let instructions = [
+            Load(true),
+            Return
+        ];
+        let result = Machine::new().run(&instructions);
+        assert_eq!(result, Some(true));
+    }
+
+    #[test]
+    fn test_write_readback() {
+        let instructions = [
+            Load(true),
+            Write(0),
+            Read(0),
+            Return
+        ];
+        let result = Machine::new().run(&instructions);
+        assert_eq!(result, Some(true));
+    }
 }
